33dc59cd2f9fa5494ff666e961ccac98b070cdeb
Zhoud <Zhoud@163.com>
2022-06-14 15:38:18 +0800

    second commit

--- src/main/java/com/athust/git/GitTest.java
+++ src/main/java/com/athust/git/GitTest.java
@@ -3,5 +3,6 @@ package com.athust.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("git 6666");
     }
 }
