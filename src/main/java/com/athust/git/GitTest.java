fbe33086ed86fd66c0dd192ae15efb6cd0507908
Zhoud <Zhoud@163.com>
2022-06-14 15:44:00 +0800

    third commit

--- src/main/java/com/athust/git/GitTest.java
+++ src/main/java/com/athust/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("git 6666");
+        System.out.println("33333");
     }
 }
